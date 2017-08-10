b7f32d7f2a333ce7bbf0064f91d6d25f4717973e
akihigaki <30319791+akihigaki@users.noreply.github.com>
2017-08-10 13:50:39 -0400

    Barcode API integration

--- src/pages/barcode/barcode.ts
+++ src/pages/barcode/barcode.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import { BarcodeScanner, BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
 
@@ -10,11 +12,20 @@ import { BarcodeScanner, BarcodeScannerOptions} from '@ionic-native/barcode-scan
 })
 
 export class BarcodePage {
-  constructor(public navCtrl: NavController, private barcode: BarcodeScanner) {
-  }
+  
+  barcode_output: any;
+  item_name: any;
+  barcode_api = "http:\/\/api.upcdatabase.org/json/bf2a7fdcdf1ea5ca41f150997431ab0b/";
+  list_of_things = ["paper", "food items", "water bottle"];
+  searching_string = "";
+  full_url: any;
+  string_parts: string[];
 
   options: BarcodeScannerOptions;
   results: {};
+
+  constructor(public navCtrl: NavController, private barcode: BarcodeScanner, public http:Http) {
+  }
 
   async scanBarcode(){
 
@@ -24,4 +35,35 @@ export class BarcodePage {
     this.results = await this.barcode.scan(this.options);
     console.log(this.results);
   }
+
+  NumberToName() {
+    this.full_url = this.barcode_api.concat(this.results["text"]);
+    this.barcode_output = this.http.get(this.full_url);
+    this.barcode_output
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+      if(data.itemname == ""){
+        this.item_name = data.description
+      }
+      else {
+        this.item_name = data.itemname;
+      }
+      
+    })
+  }
+
+  processString() {
+    alert("Processing Started With: " + this.item_name)
+    this.searching_string = this.item_name.toLowerCase();
+    this.searching_string = this.searching_string.replace(/[0-9.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    this.string_parts = this.searching_string.split(" ");
+    for (let entry of this.string_parts) {
+      if (entry.length <= 2) {
+        var index = this.string_parts.indexOf(entry);
+        this.string_parts.splice(index, 1);
+      }
+    }
+    alert(this.string_parts);
+    }
 }
