18a5d1b35e642b386cabb652fd9888d71d6b2335
Kristin Lin <klin11@stuy.edu>
2017-08-14 09:55:46 -0400

    put my changes on jadie's work

--- source/app/app.component.ts
+++ source/app/app.component.ts
@@ -8,17 +8,43 @@ import { BackandService } from '@backand/angular2-sdk';
 @Component({
   templateUrl: 'app.html',
 })
-export class MyApp {
-  rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+export class MyApp implements OnInit {
+  rootPage:any = TabsPage;
+  res: string;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private backand: BackandService) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+  
+  getList(): void {
+    this.res = 'fetching objects...';
+    this.backand.object.getList('users').then((res: any) => {
+      this.res = `${res.data.length} objects fetched`;
+      console.log(res);
+    })
+  }
+  
+  ngOnInit(): void {
+    this.backand.init({
+      appName: 'jaksgreco',
+      anonymousToken: '6e4b143c-b3f9-4fed-9d3c-9eaeb521db72',
+      //SignUpToken: '82140e16-6d17-4b7e-bbde-b9c130d0cec1',
+    //  masterToken: '6c25d82e-f7df-4ddf-9264-2712c6364c43',
+    //  userToken: '78c423ff-767f-11e7-9066-06bcf2b21c8c',
+    //  runSocket: true,
+      mobilePlatform: 'ionic'
+    });
+    this.getList();
+  }
+  
 }
 
+
+/*
 export class AppComponent implements OnInit {
   res: string;
   constructor(private backand: BackandService) { }
@@ -44,7 +70,7 @@ export class AppComponent implements OnInit {
   }
 }
 
-
+*/
 
 
 
