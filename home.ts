6c0b893c7cd6d0689a05ac7fce1911a82746b8c1
Kristin Lin <klin11@stuy.edu>
2017-08-16 15:21:49 -0400

    Add files via upload

--- home.ts
+++ home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
 
 @Component({
   selector: 'page-home',
@@ -8,23 +10,74 @@ import { AlertController } from 'ionic-angular';
 })
 export class HomePage {
 
-  val = 0;
+	val : number = 0;
 
-  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+  constructor(	public navCtrl: NavController, 
+				private alertCtrl: AlertController, 
+				private navParams : NavParams,
+				private storage : Storage) {
+  }
+  
+  update() {
+	this.storage.get('points').then((data) => {
+		if (data != null)
+		{
+			console.log(data);
+			this.val += data;
+		} 
+	});
+	
+	this.storage.ready().then(() => {
+		this.storage.set('points', 0);
+	});
+	
   }
 
-  presentAlert() {
+  apresentAlert() {
     let alert = this.alertCtrl.create({
       title: 'Good Job',
-      subTitle: 'You just made the world a greener place!',
+      subTitle: 'You powered a TV for 3 hours!',
       buttons: ['Dismiss']
     });
     alert.present();
   }
 
-  pointsval() {
-    this.val = this.val + 1;
-    this.presentAlert();
+  ppresentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Good Job',
+      subTitle: 'You just powered a 60-watt lightbulb for 3 hours!',
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  gpresentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Good Job',
+      subTitle: 'You just powered a computer for 25 minutes!',
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  gpointsval() {
+    // this.storage.set('points', this.storage.get('points') + 1);
+	this.val += 1;
+    this.gpresentAlert();
+  }
+
+  ppointsval() {
+    // this.storage.set('points', this.storage.get('points') + 1);
+	// val = this.storage.get('points');
+	this.val += 1;
+    this.ppresentAlert();
+  }
+
+  apointsval() {
+    // this.storage.set('points', this.storage.get('points') + 1);
+	// val = this.storage.get('points');
+	this.val += 1;
+    this.apresentAlert();
   }
 
 }
